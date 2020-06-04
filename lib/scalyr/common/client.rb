67d4e9ed7c83191fadc84096020299942b999889
yanscalyr <yan@scalyr.com>
2020-06-04 14:05:48 -0700

    Update versions to 0.1.3

--- lib/scalyr/common/client.rb
+++ lib/scalyr/common/client.rb
@@ -163,7 +163,7 @@ class ClientSession
 
     post = Net::HTTP::Post.new uri_path
     post.add_field('Content-Type', 'application/json')
-    version = 'output-logstash-scalyr 0.1.2'
+    version = 'output-logstash-scalyr 0.1.3'
     post.add_field('User-Agent', version + ';' + RUBY_VERSION + ';' + RUBY_PLATFORM)
 
     if not encoding.nil?
