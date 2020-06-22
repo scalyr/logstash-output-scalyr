852e74243eba46ccb53998274c83585c53a16eaf
yanscalyr <yan@scalyr.com>
2020-06-22 12:53:34 -0700

    Review comments

--- loadtest/beats-yaml-generator.py
+++ loadtest/beats-yaml-generator.py
@@ -1,21 +1,23 @@
 import os
 
-stream = os.popen('kubectl get pods -o wide -n logstash')
-output = stream.read()
-output = output.split("\n")
-result = ""
+# This command produces an output we parse to get the logstash node IPs, example here:
+# NAME                            READY   STATUS    RESTARTS   AGE   IP              NODE                                          NOMINATED NODE   READINESS GATES
+# logstash-pool-9b468dd96-x9g9c   1/1     Running   0          81s   192.168.16.90   ip-192-168-26-84.us-west-2.compute.internal   <none>           <none>
+with os.popen('kubectl get pods -o wide -n logstash') as stream:
+    pods_string = stream.read()
+pods_list = pods_string.split("\n")
 
+hosts_list = []
 i = 0
-for o in output:
-    if i > 0 and o != "":
-        s = o.split()
-        if i >= 2:
-            result += ", "
-        result += '"%s:5044"' % s[5]
+for line in pods_list:
+    if i > 0 and line != "":
+        pod_info = line.split()
+        if pod_info[5] != "<none>":
+            hosts_list.append('"%s:5044"' % pod_info[5])
     i += 1
 
-beats = ""
-with open('k8s_yaml/beats.yaml',mode='r') as f:
-    beats = f.read()
+beats_yaml = ""
+with open('k8s_yaml/beats.yaml', mode='r') as f:
+    beats_yaml = f.read()
 
-print(beats.replace("LOGSTASH_HOSTS", result, 1))
+print(beats_yaml.replace("LOGSTASH_HOSTS", ", ".join(hosts_list), 1))
