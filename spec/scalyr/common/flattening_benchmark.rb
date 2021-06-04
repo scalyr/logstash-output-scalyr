8f0a0e17f96e2c70f98a59655515a82bbe6ccf0c
yan <yan@scalyr.com>
2021-06-03 23:44:04 -0700

    Add randomized data instead of pre-set json file

--- spec/scalyr/common/flattening_benchmark.rb
+++ spec/scalyr/common/flattening_benchmark.rb
@@ -7,15 +7,33 @@ require_relative '../../../lib/scalyr/common/util'
 # wanted to accurately measure using multiple iterations we would need te different
 # input data for each iteration.
 
-ITERATIONS = 1
+def rand_str(len)
+  return (0...len).map { (65 + rand(26)).chr }.join
+end
 
-file = File.read('./spec/fixtures/rows.json')
-data = JSON.parse(file)
+def generate_hash(widths)
+  result = {}
+  if widths.empty?
+    return rand_str(20)
+  else
+    widths[0].times do
+      result[rand_str(9)] = generate_hash(widths[1..widths.length])
+    end
+    return result
+  end
+end
+
+ITERATIONS = 100
+
+data = []
+ITERATIONS.times do
+  data << generate_hash([8, 3, 2])
+end
 
 result = []
-ITERATIONS.times {
-  result << Benchmark.measure { Scalyr::Common::Util.flatten(data) }
-}
+ITERATIONS.times do |i|
+  result << Benchmark.measure { Scalyr::Common::Util.flatten(data[i]) }
+end
 
 sum = result.inject(nil) { |sum, t| sum.nil? ? sum = t : sum += t }
 avg = sum / result.size
