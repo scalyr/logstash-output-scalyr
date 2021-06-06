1c4659a3f23855f158481390defa977f15286fd6
Tomaz Muraus <tomaz@tomaz.me>
2021-06-06 22:18:48 +0200

    Try using less iterations so we don't hit memory limits on Circle CI.

--- spec/scalyr/common/flattening_benchmark.rb
+++ spec/scalyr/common/flattening_benchmark.rb
@@ -7,7 +7,7 @@ require_relative '../../../lib/scalyr/common/util'
 # sense since it will just use JITed version of the code which will be very fast. If we
 # wanted to accurately measure using multiple iterations we would need te different
 # input data for each iteration.
-ITERATIONS = 1000
+ITERATIONS = 800
 
 def rand_str(len)
   return (0...len).map { (65 + rand(26)).chr }.join
