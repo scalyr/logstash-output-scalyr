7e75294b980022c5c3fba061dbd3251c7a302498
Tomaz Muraus <tomazm@sentinelone.com>
2021-09-12 21:25:51 +0200

    Remove debug change.

--- lib/logstash/outputs/scalyr.rb
+++ lib/logstash/outputs/scalyr.rb
@@ -378,8 +378,6 @@ class LogStash::Outputs::Scalyr < LogStash::Outputs::Base
     begin
       @client_session.send_ping(body)
     rescue Scalyr::Common::Client::ClientError, Manticore::ResolutionFailure => e
-      puts "---1"
-      puts e.message
       if not e.message.nil? and (e.message.include?("nodename nor servname provided") or
           e.message.downcase.include?("name or service not know"))
         raise LogStash::ConfigurationError,
@@ -400,8 +398,6 @@ class LogStash::Outputs::Scalyr < LogStash::Outputs::Base
       end
 
     rescue => e
-      puts "---2"
-      puts e.message
       @logger.warn("Received non-fatal error during connectivity check", :error => e.message)
     end
   end
