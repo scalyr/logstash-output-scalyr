8f832dc5561974f5f2081eb400f8dcfcf8fa224e
Tomaz Muraus <tomazm@sentinelone.com>
2022-07-29 18:49:54 +0200

    Prepare version for the next release.

--- lib/scalyr/constants.rb
+++ lib/scalyr/constants.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-PLUGIN_VERSION = "v0.2.5.beta"
+PLUGIN_VERSION = "v0.2.6.beta"
 
 # Special event level attribute name which can be used for setting event level serverHost attribute
 EVENT_LEVEL_SERVER_HOST_ATTRIBUTE_NAME = '__origServerHost'
